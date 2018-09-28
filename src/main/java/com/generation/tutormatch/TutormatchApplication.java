c1a60be5c2b74a1f41512aaa1d5347139c2fc891
shakyhip <shakyhip@users.noreply.github.com>
2018-09-28 11:24:07 -0500

    first commit

---
+++ src/main/java/com/generation/tutormatch/TutormatchApplication.java
@@ -0,0 +1,12 @@
+package com.generation.tutormatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutormatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutormatchApplication.class, args);
+    }
+}
